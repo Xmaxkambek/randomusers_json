8faabe0ab95b14f7566be76454f4f7d8ea27ef8f
ZARIFMAC <uzbzarif@gmail.com>
2022-11-22 15:29:01 +0500

    read user

--- bin/randomusers_json.dart
+++ bin/randomusers_json.dart
@@ -1,3 +1,21 @@
 import 'package:randomusers_json/randomusers_json.dart';
+import 'dart:io';
+import 'dart:convert';
 
-void main() {}
+// Define a function that reads a JSON data from a file.
+Map fromFile(String filename) {
+  final file = File(filename);
+  final json = file.readAsStringSync();
+  return jsonDecode(json);
+}
+
+void main() {
+// File path
+
+  final path = 'users_1.json';
+
+  Map data = fromFile(path);
+
+  List users = data['users'];
+  print(users[0]);
+}
